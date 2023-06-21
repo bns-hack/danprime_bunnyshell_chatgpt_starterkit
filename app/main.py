2cecf0c2ddd73fe1449f79cb3eadb43eba9daf57
Daniel Tse <daniel.tse@sectigo.com>
2023-06-21 18:45:38 -0400

    Add OpenAI / ChatGPT integration

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import os
+import openai
+openai.api_key=os.getenv('OPENAPI_KEY')
+
 class Prompt(BaseModel):
     prompt: str
 
@@ -22,6 +26,20 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None]=None):
     return {"item_id": item_id, "q":q}
+
+@app.get("/chatgpttest")
+async def read_root():
+    model_engine = "text-davinci-003"
+    prompt = "Hello, how are you today?"
+    completion = openai.Completion.create(
+            engine=model_engine,
+            prompt=prompt,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.5,
+            )
+    return completion
 
 @app.post("/prompt")
 async def incoming_prompt(prompt: Prompt):
